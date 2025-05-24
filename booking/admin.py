ea604d40f9bb6985866f1f307bbb0ec9d32c9b83
muhammad usman <usmankhadim.uk.uk@gmail.com>
2025-05-24 14:13:36 +0500

    3 - Models and register the models in admin panel

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import Bus, Seat, Booking
 # Register your models here.
+
+
+class BusAdmin(admin.ModelAdmin):
+    list_display = ('bus_name', 'number', 'origin', 'destination', 'start_time', 'reach_time', 'no_of_seats', 'price')
+
+
+class SeatAdmin(admin.ModelAdmin):
+    list_display = ('seat_number', 'bus', 'is_booked')
+
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bus', 'seat', 'booking_time', 'origin','price')
+
+
+admin.site.register(Bus, BusAdmin)
+admin.site.register(Seat, SeatAdmin)
+admin.site.register(Booking, BookingAdmin)
